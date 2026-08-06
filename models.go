3baed40ff52d7e66c0a36b155142011ceaf9c1e9
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2026-08-06 16:12:37 +0300

    add fields to the todo model struct

--- models.go
+++ models.go
@@ -1,7 +1,15 @@
 package main
 
+import "time"
+
 type Todo struct {
-	ID        uint   `gorm:"primaryKey" json:"id"`
-	Title     string `gorm:"not null" json:"title"`
-	Completed bool   `gorm:"default:false" json:"completed"`
+	ID          uint       `gorm:"primaryKey" json:"id"`
+	Title       string     `gorm:"not null" json:"title"`
+	Completed   bool       `gorm:"default:false" json:"completed"`
+	Category    string     `gorm:"default:'General'" json:"category"`
+	Piority     string     `gorm:"default:'Medium'" json:"priority"`
+	CompletedAt *time.Time `json:"completedAt"`
+	DueDate     *time.Time `json:"dueDate"`
 }
+
+// used pointers to *time.Time so value can be nil and to make comparisons between dates easier than string.
