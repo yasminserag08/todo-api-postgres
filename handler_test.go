ae747a6c989e54e234b95862bed267366cf71705
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2026-08-09 13:42:19 +0300

    Add all tests for GET /todos

--- handler_test.go
+++ handler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http/httptest"
 	"testing"
 
@@ -8,7 +9,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetAll(t *testing.T) {
+func TestGetAll_Success(t *testing.T) {
+	testRepo := new(MockTodoRepository)
+
+	testRepo.On("GetAll").Return([]Todo{{ID: 1, Title: "Test Todo"}}, nil)
+
+	req := httptest.NewRequest("GET", "/todos", nil)
+
+	w := httptest.NewRecorder()
+
+	c, _ := gin.CreateTestContext(w)
+	c.Request = req
+
+	handler := NewHandler(testRepo)
+	handler.GetAll(c)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Contains(t, w.Body.String(), "Test Todo")
+
+}
+
+func TestGetAll_EmptyArray(t *testing.T) {
 	// create an instance of repository mock
 	testRepo := new(MockTodoRepository)
 
@@ -27,4 +48,26 @@ func TestGetAll(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Contains(t, "[]", w.Body.String())
+}
+
+func TestGetAll_DatabaseError(t *testing.T) {
+	// create an instance of repository mock
+	testRepo := new(MockTodoRepository)
+
+	// control what mock returns
+	testRepo.On("GetAll").Return([]Todo{}, errors.New("db error")) // simulate that repo returns an error
+
+	req := httptest.NewRequest("GET", "/todos", nil)
+
+	w := httptest.NewRecorder()
+
+	c, _ := gin.CreateTestContext(w)
+	c.Request = req
+
+	handler := NewHandler(testRepo)
+	handler.GetAll(c)
+
+	assert.Equal(t, 500, w.Code)
+	assert.Contains(t, w.Body.String(), "db error")
+
 }
